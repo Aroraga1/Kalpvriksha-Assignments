13aa894f89f7134fb78bdf7237392ae0c9fe96df
GARVIT ARORA <154904721+Aroraga1@users.noreply.github.com>
2025-11-29 20:00:42 +0530

    Resolve all required changes

--- Assignment1.c
+++ Assignment1.c
@@ -4,93 +4,128 @@
 
 char getGrade(int avg){
     char grade;
-
-    if(avg>=85) grade='A';
-    else if(avg>=70) grade='B';
-    else if(avg>=50) grade='C';
-    else if(avg>=35) grade='D';
-    else grade='F';
-
+    if(avg>=85) {
+        grade='A';
+    }
+    else if(avg>=70){
+        grade='B';
+    }
+    else if(avg>=50){
+        grade='C';
+    }
+    else if(avg>=35){
+        grade='D';
+    } 
+    else {
+        grade='F';
+    }
     return grade;
 }
 
-typedef struct StudentData{
+typedef struct studentMarksData{
     int id;
     char name[50];
     int marks[3];
     int total;
     float average;
     char grade; 
-} st;
+} data;
 
-void assignValues(st *st){
-    st->total=0;
-    for(int i=0; i<3; i++) st->total+=st->marks[i];
-    st->average= (float) st->total/3;
-    st->grade=getGrade(st->average);
+void assignValues(data *data){
+    data->total=0;
+    for(int i=0; i<3; i++){
+        data->total+=data->marks[i];
+    } 
+    data->average= (float) data->total/3;
+    data->grade=getGrade(data->average);
 }
 
-void clearBuffer(){
-    char c;
-    while(c = getchar() != '\n');
+void clearBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
 }
 
-void ContinueOperation(){
-    st st[MAX_STUDENT];
+void printData(int temp, data dataArray[]){
+    printf("\n");
+        for(int i=0; i<temp; i++) {
+            printf("Roll: %d\n", dataArray[i].id);
+            printf("Name: %s\n", dataArray[i].name);
+            printf("Total: %d\n", dataArray[i].total);
+            printf("Average: %f\n", dataArray[i].average);
+            printf("Grade: %c\n", dataArray[i].grade);
+            switch (dataArray[i].grade)
+            {
+            case 'A':
+                printf("Performance: *****");
+                break;
+            case 'B':
+                printf("Performance: ****");
+                break;
+            case 'C':
+                printf("Performance: ***");
+                break;
+            case 'D':
+                printf("Performance: **");
+                break;
+            default: 
+                break;
+
+            }
+            printf("\n\n");
+        }
+    printf("\n");
+}
+
+void evaluateStudentRecord(){
+    data dataArray[MAX_STUDENT];
     int temp;
     printf("Stop(0) / Number of entries: ");
     if(scanf("%d", &temp) != 1){
-        printf("Wrong Entry! Enter specific numbers of Entries\n");
+        printf("Wrong Entry! Enter specific numbers in Entries\n");
         clearBuffer();
-        ContinueOperation();
+        evaluateStudentRecord();
     }
-    if (temp < 0 || temp > 100) {
-    printf("\n id %d is out of range (0-100)\n");
+    if (temp < 0 || temp > MAX_STUDENT) {
+    printf("\nInvalid Entry\n");
+    clearBuffer();
+    evaluateStudentRecord();
+    return;
     }
     if(temp==0) return;
     printf("Enter your Entry as -> id name Mark1 Mark2 Mark3\n");
-
     //Taking input till the number of entries..
             for(int i=0; i<temp; i++){
                 
-                if (scanf("%d", &st[i].id) != 1) {
+                if (scanf("%d", &dataArray[i].id) != 1) {
                 printf("\nInvailid Entry!\n");
                 break; 
                 }
                 
-                if (scanf("%49s", st[i].name) != 1) { 
+                if (scanf("%49s", dataArray[i].name) != 1) { 
                 printf("\nInvailid Entry!.\n");
                 i--;
                 continue;
                 }
 
             for(int j=0; j<3; j++){
-                if (scanf("%d", &st[i].marks[j]) != 1) {
+                if (scanf("%d", &dataArray[i].marks[j]) != 1) {
                 printf("\nInvailid Entry!\n", j + 1);
                 break; 
             }
-            if (st[i].marks[j] < 0 || st[i].marks[j] > 100) {
+            if (dataArray[i].marks[j] < 0 || dataArray[i].marks[j] > 100) {
                 printf("\nInvailid Entry!\n", j + 1);
                 break; 
             }
             }
-            printf("\n");
-            assignValues(&st[i]);
+            assignValues(&dataArray[i]);
         }
-        for(int i=0; i<temp; i++) {
-            printf("Roll: %d\n", st[i].id);
-            printf("Name: %s\n", st[i].name);
-            printf("Total: %d\n", st[i].total);
-            printf("Average: %f\n", st[i].average);
-            printf("Grade: %c\n", st[i].grade);
-            printf("\n")
-        }
-        ContinueOperation();
+        printData(temp,dataArray);
+        evaluateStudentRecord();
 }
 
+
 int main(){
-    
-    ContinueOperation();
-    
+    printf("*Student Marks eveluation system*\n");
+    evaluateStudentRecord();
     return 0;
 }
