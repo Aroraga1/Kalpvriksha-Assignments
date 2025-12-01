6ebd7ec363d59597f49b9109ea2e2f051519a291
GARVIT ARORA <154904721+Aroraga1@users.noreply.github.com>
2025-12-01 16:06:30 +0530

    corrected code

--- Assignment2.c
+++ Assignment2.c
@@ -1,154 +1,100 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
 #define MAX_SIZE 10
-#define INTENSITY_MAX 255
+#define INTENSITY_MAX 255 
+int sonarImage[MAX_SIZE][MAX_SIZE]; 
+int tempImage[MAX_SIZE][MAX_SIZE];
 
-int sonarImage[MAX_SIZE * MAX_SIZE]; 
-
-unsigned long customSeed = 1; 
-
-int customRandom() {
-    return rand(); 
-}
-
-void askForSizeAndStart(int *sizeToUse);
-void printImage(int *dataPointer, int sizeOfImage);
-void generateRandomImage(int *dataPointer, int sizeOfImage);
-void performRotation(int *dataPointer, int sizeOfImage);
-void applySmoothing(int *dataPointer, int sizeOfImage);
-void swap(int *pointerFirst, int *pointerSec);
-
-void askForSizeAndStart(int *sizeToUse) {
-    int userValue = 0;
-    while (userValue < 2 || userValue > MAX_SIZE) {
-        printf("Enter image size (2-%d): ", MAX_SIZE);
-        if (scanf("%d", &userValue) != 1) {
-            while(getchar() != '\n'); 
-        }
-        if (userValue < 2 || userValue > MAX_SIZE) {
-            printf("Invalid size. Must be between 2 and %d.\n", MAX_SIZE);
-        }
-    }
-    *sizeToUse = userValue;
-}
-
-void printImage(int *dataPointer, int sizeOfImage) {
-    int N = sizeOfImage;
-    for (int row = 0; row < N; row++) {
-        for (int col = 0; col < N; col++) {
-            int *currentPtr = dataPointer + (row * N) + col;
-            int value = *currentPtr;
-            printf("%4d", value);
-        }
-        printf("\n");
-    }
-}
-
-void generateRandomImage(int *dataPointer, int sizeOfImage) {
-    int N = sizeOfImage;
-    for (int row = 0; row < N; row++) {
-        for (int col = 0; col < N; col++) {
-            int *currentPtr = dataPointer + (row * N) + col;
-            *currentPtr = customRandom() % (INTENSITY_MAX + 1);
+void generateMatrix(int N){
+    int *ptr = (int *)sonarImage; 
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            *(ptr + (i * MAX_SIZE) + j) = rand() % (INTENSITY_MAX + 1); 
         }
     }
 }
 
-void swap(int *pointerFirst, int *pointerSec) {
-    int tempValue = *pointerFirst;
-    *pointerFirst = *pointerSec;
-    *pointerSec = tempValue;
-}
-
-void performRotation(int *dataPointer, int sizeOfImage) {
-    int N = sizeOfImage;
-    
-    for (int row = 0; row < N; row++) {
-        for (int col = row + 1; col < N; col++) {
-            int *p_rc = dataPointer + (row * N) + col;
-            int *p_cr = dataPointer + (col * N) + row;
-            swap(p_rc, p_cr);
+void printImage(int (*ptr)[MAX_SIZE], int N){ 
+    int *base_ptr = (int *)ptr; 
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            printf("%4d", *(base_ptr + (i * MAX_SIZE) + j)); 
         }
-    }
-
-    for (int row = 0; row < N; row++) {
-        for (int col = 0; col < N / 2; col++) {
-            int *p_left = dataPointer + (row * N) + col;
-            int *p_right = dataPointer + (row * N) + (N - 1 - col);
-            swap(p_left, p_right);
-        }
+        printf("\n"); 
     }
 }
 
-void applySmoothing(int *dataPointer, int sizeOfImage) {
-    int N = sizeOfImage;
-    int originalRow[MAX_SIZE]; 
+void swap(int *ptr1, int *ptr2){
+    int temp = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = temp;
+}
 
-    for (int row = 0; row < N; row++) {
-        
-        for (int col = 0; col < N; col++) {
-             int *currentPtr = dataPointer + (row * N) + col;
-             originalRow[col] = *currentPtr;
+void rotateMatrix(int (*ptr)[MAX_SIZE], int N){
+    int *ptrf = (int *)ptr;
+    for(int i=0; i<N; i++){
+        for(int j=i+1; j<N; j++){
+            swap(ptrf + (i * MAX_SIZE) + j, ptrf + (j * MAX_SIZE) + i);
         }
+    }
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N/2; j++){
+            swap(ptrf + (i * MAX_SIZE) + j, ptrf + (i * MAX_SIZE) + (N - 1 - j));
+        }
+    }
+}
 
-        for (int col = 0; col < N; col++) {
-            long totalIntensity = 0;
-            int neighborCount = 0;
-
-            for (int rOffset = -1; rOffset <= 1; rOffset++) {
-                for (int cOffset = -1; cOffset <= 1; cOffset++) {
-                    int neighborRow = row + rOffset;
-                    int neighborCol = col + cOffset;
-
-                    if (neighborRow >= 0 && neighborRow < N && neighborCol >= 0 && neighborCol < N) {
-                        int neighborValue;
-
-                        if (neighborRow < row) {
-                            int *neighborPtr = dataPointer + (neighborRow * N) + neighborCol;
-                            neighborValue = *neighborPtr;
-                        } else if (neighborRow == row) {
-                            neighborValue = originalRow[neighborCol];
-                        } else { 
-                            int *neighborPtr = dataPointer + (neighborRow * N) + neighborCol;
-                            neighborValue = *neighborPtr;
-                        }
-                        
-                        totalIntensity += neighborValue;
-                        neighborCount++;
+void applySmoothing(int (*ptr)[MAX_SIZE], int N){
+    int *ptr_in = (int *)ptr; 
+    int *ptr_out = (int *)tempImage; 
+    int i,j;
+    for(int raw = 0; raw < N; raw++){
+        for(int col = 0; col < N; col++){
+            long long sum = 0;
+            int count = 0;
+            for(int ki = -1; ki <= 1; ki++){
+                for(int kj = -1; kj <= 1; kj++){
+                    i = raw + ki; 
+                    j = col + kj;        
+                    if(i >= 0 && i < N && j >= 0 && j < N){
+                        int index = i * MAX_SIZE + j;
+                        sum += *(ptr_in + index);
+                        count++;
                     }
                 }
             }
-
-            int newIntensity = (int)((float)totalIntensity / (float)neighborCount + 0.5f);
-            
-            int *currentPlacePtr = dataPointer + (row * N) + col;
-            *currentPlacePtr = newIntensity;
+            int avg = (int)(sum / count);
+            int output_index = raw * MAX_SIZE + col;
+            *(ptr_out + output_index) = avg;
+        }
+    }
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            int index = i * MAX_SIZE + j;
+            *(ptr_in + index) = *(ptr_out + index);
         }
     }
 }
 
-int main() {
+int main(){
     srand(time(NULL)); 
-    
-    int imageSize = 0;
-    
-    askForSizeAndStart(&imageSize);
-    
-    int *dataPointer = sonarImage; 
-    
-    generateRandomImage(dataPointer, imageSize);
-    printf("Original Randomly Generated Matrix:\n");
-    printImage(dataPointer, imageSize);
-    
-    performRotation(dataPointer, imageSize);
+    int imageSize;
+    printf("Enter matrix size (2-10): ");
+    if (scanf("%d", &imageSize) != 1 || imageSize < 2 || imageSize > MAX_SIZE) {
+        printf("Invalid size or size < 3 entered. Filtering requires N >= 3.\n");
+        return 0;
+    }
+    generateMatrix(imageSize);
+    printf("\nOriginal Randomly Generated Matrix:\n");
+    printImage(sonarImage, imageSize); 
     printf("\nMatrix after 90 degree Clockwise Rotation:\n");
-    printImage(dataPointer, imageSize);
-    
-    applySmoothing(dataPointer, imageSize);
-    printf("\nMatrix after Applying 3x3 Smoothing Filter:\n");
-    printImage(dataPointer, imageSize);
-    
-    return 0;
+    rotateMatrix(sonarImage,imageSize);
+    printImage(sonarImage,imageSize);
+    printf("\n");
+    applySmoothing(sonarImage, imageSize);
+    printf("\nMatrix after Applying 3*3 Smoothing Filter:\n");
+    printImage(sonarImage, imageSize); 
+    return 0; 
 }
